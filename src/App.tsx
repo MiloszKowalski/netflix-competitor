ff3cb587e729745c2ba1cb2a80eb2613e046d161
Miłosz Kowalski <milosz_kowalski44@wp.pl>
2020-11-17 22:17:27 +0100

    Update App to reflect changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 
+import Footer from 'components/Footer';
+import Home from 'views/Home';
 import TopMenu from 'components/TopMenu';
 import TopMenuContextProvider from 'contexts/TopMenuContext';
+import MovieContextProvider from 'contexts/MovieContext';
 
 function App() {
   return (
     <TopMenuContextProvider>
-      <div className="App">
-        <TopMenu />
-      </div>
+      <MovieContextProvider>
+        <div className="App">
+          <TopMenu />
+          <Home />
+          <Footer />
+        </div>
+      </MovieContextProvider>
     </TopMenuContextProvider>
   );
 }
