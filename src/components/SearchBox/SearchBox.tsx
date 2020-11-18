8968de06664675ebc85fbf0bacd3d630f8a958d2
Miłosz Kowalski <milosz_kowalski44@wp.pl>
2020-11-18 04:53:22 +0100

    Wire up search box for filters

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,5 +1,6 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useContext } from 'react';
 
+import { MovieContext } from 'contexts/MovieContext';
 import { ReactComponent as SearchIcon } from 'svg/icons/SearchIcon.svg';
 
 import './SearchBox.scss';
@@ -7,6 +8,7 @@ import './SearchBox.scss';
 const SearchBox: React.FC = () => {
   const [expanded, setExpanded] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
+  let { searchFilter, setSearchFilter } = useContext(MovieContext);
 
   function expandSearch() {
     if (!inputRef.current) return;
@@ -21,11 +23,16 @@ const SearchBox: React.FC = () => {
     }
   }
 
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setSearchFilter(e.target.value);
+  }
+
   return (
     <div className={`SearchBox ${ expanded ? 'expanded': '' }`}
       onFocus={ expandSearch }>
       <SearchIcon onClick={ expandSearch } />
       <input ref={ inputRef } onBlur={ hideSearch }
+        value={ searchFilter } onChange={ (e) => handleChange(e) }
         placeholder="Titles, people, genres" type="text"/>
     </div>
   )
