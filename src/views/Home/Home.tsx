57eae2b08791ba5690eb71811559c1f1111f44c7
Miłosz Kowalski <milosz_kowalski44@wp.pl>
2020-11-18 04:59:11 +0100

    Add movie fetching and filtering and wire up featured section into Home view

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import './Home.scss';
 
+import FeaturedMovie from 'components/FeaturedMovie';
 import MovieList from 'components/MovieList';
 import RecomendationFilters from 'components/RecomandationFilters';
 
+import { MovieContext } from 'contexts/MovieContext';
+import { getMovieListing, MovieInfo } from 'utils/apiHandler';
+
 const Home: React.FC = () => {
+  const { updateMovieList, movies, searchFilter, enabledGenres } = useContext(MovieContext);
+
+  const filteredMovies = movies.filter(x => {
+    const movieIncludesName = x.name.toLowerCase().includes(searchFilter.toLowerCase());
+    let movieIncludesGenre = true;
+    if(enabledGenres.length) {
+      movieIncludesGenre = !!enabledGenres.find(y => y === x.category);
+    }
+
+    return movieIncludesName && movieIncludesGenre;
+  });
+
+  useEffect(() => {
+    getMovieListing()
+    .then((movies: MovieInfo[]) => {
+      updateMovieList(movies);
+    });
+  }, [updateMovieList]);
+
+
   return (
     <main className="Home">
       <RecomendationFilters />
-      <MovieList />
+      <FeaturedMovie />
+      <MovieList movies={ filteredMovies } />
     </main>
   )
 }
